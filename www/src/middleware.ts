1eaed29c1550af06470124166d4532c9e8a74374
Armin Emmert <emmertarmin@gmail.com>
2024-06-12 01:28:36 +0200

    fix redirect loop issue

--- www/src/middleware.ts
+++ www/src/middleware.ts
@@ -2,8 +2,8 @@ import { defineMiddleware } from 'astro/middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
   if (
-    !context.url.pathname.endsWith('/login')
-    && !context.url.pathname.endsWith('/register')
+    !/\/login\/?$/.test(context.url.pathname)
+    && !/\/register\/?$/.test(context.url.pathname)
     && !context.cookies.has('pbToken')
   ) {
     return context.redirect('/login', 303)
