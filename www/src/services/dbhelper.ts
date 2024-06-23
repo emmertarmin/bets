e95b19d15d24dca93a3c37bcc4beabcedf208a3d
Armin Emmert <emmertarmin@gmail.com>
2024-06-23 20:46:15 +0200

    fix undefined bets and api perPage hard limit

--- www/src/services/dbhelper.ts
+++ www/src/services/dbhelper.ts
@@ -13,7 +13,7 @@ export const getUser = async (pbToken: string) => {
 }
 
 export const updateUser = async(pbToken: string, data: any) => {
-    console.log(pbToken, data);
+    // console.log(pbToken, data);
     return await pbPATCH(`/api/collections/users/records/${getUserId(pbToken)}`
     , data
     , pbToken).then((res) => res );
@@ -21,7 +21,7 @@ export const updateUser = async(pbToken: string, data: any) => {
 
 export const getAllUsers = async (pbToken: string) => {
     return pbGET("/api/collections/users/records", {
-        perPage: 200
+        perPage: 500 // 500 is the hard limit
     }, pbToken).then((res) => res.data.items.toSorted((a: any, b: any) => ((b.points - a.points) || (a.username.localeCompare(b.username)))));
 }
 
@@ -29,21 +29,31 @@ export const getMatches = async (pbToken: string) => {
     return await pbGET('/api/collections/games/records', {
         sort: 'date',
         expand: 'team_1,team_2',
-        perPage: 200
+        perPage: 500 // 500 is the hard limit
     }, pbToken).then(res => res.data.items)
 }
 
 export const getAllBets = async (pbToken: string) => {
-    return await pbGET('/api/collections/bets/records', {
-        perPage: 500,
+    const betsData = await pbGET('/api/collections/bets/records', {
+        perPage: 500, // 500 is the hard limit
         expand: 'user,game'
-    }, pbToken).then(res => res.data.items)
+    }, pbToken).then(res => res.data)
+    if (betsData.totalPages === 1) return betsData.items
+    for (const page of Array.from({ length: betsData.totalPages - 1 }, (_, i) => i + 2)) {
+        const pageData = await pbGET('/api/collections/bets/records', {
+            perPage: 500, // 500 is the hard limit
+            page,
+            expand: 'user,game'
+        }, pbToken).then(res => res.data)
+        betsData.items.push(...pageData.items)
+    }
+    return betsData.items
 }
 
 export const getUserBets = async (pbToken: string) => {
     return await pbGET('/api/collections/bets/records', {
         filter: `user="${getUserId(pbToken)}"`,
-        perPage: 200
+        perPage: 500 // 500 is the hard limit
     }, pbToken).then(res => res.data.items)
 }
 
@@ -51,6 +61,6 @@ export const getGroups = async (pbToken: string) => {
     return await pbGET('/api/collections/groups/records', {
         sort: 'name',
         expand: 'team_1,team_2,team_3,team_4',
-        perPage: 200
+        perPage: 500 // 500 is the hard limit
     }, pbToken).then(res => res.data.items)
 }
